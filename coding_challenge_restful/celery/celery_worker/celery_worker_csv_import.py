e39bec2a3392cdbb689ea10ae2dab0c43212d753
nalawadepiyush <nalawadepiyush@gmail.com>
2019-06-18 23:12:28 +0530

    Filenotfound error wip

--- coding_challenge_restful/celery/celery_worker/celery_worker_csv_import.py
+++ coding_challenge_restful/celery/celery_worker/celery_worker_csv_import.py
@@ -15,8 +15,8 @@ def task_csv_import(self, *args, **kwargs):
     file_id = self.async_task_obj.payload.get('id')
     csv_object = self.db.query(BulkCSVUpload).filter(BulkCSVUpload.id == file_id).first()
     path_id = csv_object.csv.file_id
-    abs_path = os.path.abspath('files')
-    content = open(abs_path + '/{path_id}/file'.format(path_id=path_id), 'rb')
+    current_dir = os.getcwd()
+    content = open(current_dir + '/{path_id}/file'.format(path_id=path_id), 'rb')
     products_csv_object = content.read().decode('utf-8')
     reader = csv.DictReader(
         products_csv_object.splitlines(),
