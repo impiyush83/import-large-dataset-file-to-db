8cc4cba5cb13044d6db982b156dc50a4feef25a0
Piyush Nalawade <piyush.nalawade@exadatum.com>
2019-06-20 13:14:41 +0530

    Removed & from procfile worker command that caused the worker app to fail

--- worker_start.sh
+++ worker_start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-celery worker -A coding_challenge_restful.celery.celery_worker.celery_worker_csv_import -l info -c 1 -Ofair -Q products_csv_import &
+celery worker -A coding_challenge_restful.celery.celery_worker.celery_worker_csv_import -l info -c 1 -Ofair -Q products_csv_import
